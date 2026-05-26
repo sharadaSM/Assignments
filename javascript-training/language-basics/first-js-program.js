8a34549c1933336bb4553b0e9b1c789e617064fb
Sharada Motigi <sharada.motigi@gmail.com>
2026-05-26 10:46:04 +0530

    first commit

---
+++ javascript-training/language-basics/first-js-program.js
@@ -0,0 +1,24 @@
+/*console.log("Hello, world!");
+
+let Employee = {
+  name: "Preeti",
+  age: 30,
+  department: "Engineering"
+};
+
+
+console.log(Employee.name);
+console.log(Employee["department"]);
+console.log(Employee.age) */
+
+getdate = new Date();
+console.log(getdate);
+
+getdate = new Date();
+console.log(getdate.getMonth() + 1);  
+
+getdate = new Date();
+console.log(getdate.getFullYear());
+
+getdate = new Date();
+console.log(getdate.getDay());
---
+++ typescript-training/data-types.ts
@@ -0,0 +1,10 @@
+let emplid : number = 12345;
+let empname : string = "Preeti";
+let isPermanent : boolean = true;
+let department : string[] = ["HR", "Finance", "IT"];
+let empDetails : [number, string, boolean] = [12345, "Preeti", true];
+console.log(emplid);
+console.log('Name of the empname is ${empname}');
+console.log(isPermanent);
+console.log(department);
+console.log(empDetails);
